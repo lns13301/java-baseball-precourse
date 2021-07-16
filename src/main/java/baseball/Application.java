de1311ad37cbe5d8137261639225d0b612ec469c
lns13301 <rkdrbgks22@ks.ac.kr>
2021-07-17 02:18:04 +0900

    feat: 애플리케이션 동작 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,14 @@
 package baseball;
 
+import baseball.controller.BaseballController;
+
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO 구현 진행
+        BaseballController baseballController = new BaseballController();
+
+        baseballController.run(scanner);
     }
 }
